ee787c4994146751298f40de0c1b5dd001c2ab30
tusigeorgia <tusigeorgia.gt@gmail.com>
2019-03-02 14:37:18 +0300

    merge commit

--- app/src/main/java/com/myfarmnow/myfarmcrop/activities/MainActivity.java
+++ app/src/main/java/com/myfarmnow/myfarmcrop/activities/MainActivity.java
@@ -9,6 +9,8 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.myfarmnow.myfarmcrop.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
