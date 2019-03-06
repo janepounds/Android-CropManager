ed9ffb0286729d8b162272bca713810ab7a0b55f
Kasumba Robert <robein@ymail.com>
2019-03-06 12:57:28 +0300

    Started on the insertion of fields list activity

--- app/src/main/java/com/myfarmnow/myfarmcrop/activities/CropInventorySeedsManagerActivity.java
+++ app/src/main/java/com/myfarmnow/myfarmcrop/activities/CropInventorySeedsManagerActivity.java
@@ -6,11 +6,17 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.myfarmnow.myfarmcrop.R;
+import com.myfarmnow.myfarmcrop.models.CropInventorySeeds;
 
 public class CropInventorySeedsManagerActivity extends AppCompatActivity {
 
+    CropInventorySeeds seedsInventoryToEdit=null;
+    EditText purchaseDatTxt, seedNameTxt,varietyTxt,dressingTxt,tgwTxt,quantityTxt,costTxt;
+    Spinner usageUnitSpinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,14 +24,21 @@ public class CropInventorySeedsManagerActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+       //txt_crop_purchase_date
+       //
+       //txt_crop_variety
+       //txt_crop_dressing
+       //txt_crop_tgw
+       //txt_crop_usage_unit
+       //txt_crop_quantity
+       //txt_crop_cost
+
+
+    }
+
+    public void initializeForm(){
+        purchaseDatTxt = findViewById(R.id.txt_crop_purchase_date);
+        seedNameTxt = findViewById(R.id.txt_crop_seed_name);
     }
 
 }
