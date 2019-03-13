3edbd1655eae6c6dcd6bf3b28ee759aae5c009db
tusigeorgia <tusigeorgia.gt@gmail.com>
2019-03-13 14:48:22 +0300

    Merging more interfaces

---
+++ app/src/main/java/com/myfarmnow/myfarmcrop/activities/CropProductManagerActivity.java
@@ -0,0 +1,15 @@
+package com.myfarmnow.myfarmcrop.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.myfarmnow.myfarmcrop.R;
+
+public class CropProductManagerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crop_product_manager);
+    }
+}
