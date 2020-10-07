bf4f0e2ff5398c09c1cd9b5c98857a4f072a4b7f
JovanDon <mutesasirajovan@gmail.com>
2020-10-07 15:28:57 +0300

    modified crop activities

--- app/src/main/java/com/myfarmnow/myfarmcrop/adapters/farmrecords/CropActivitiesListRecyclerAdapter.java
+++ app/src/main/java/com/myfarmnow/myfarmcrop/adapters/farmrecords/CropActivitiesListRecyclerAdapter.java
@@ -89,7 +89,7 @@ public class CropActivitiesListRecyclerAdapter extends RecyclerView.Adapter< Rec
             fertilizerApplicationHolder.costTextView.setText(CropSettingsSingleton.getInstance().getCurrency()+" "+ NumberFormat.getInstance().format(fertilizerApplication.getCost())); //TODO replace currency
             fertilizerApplicationHolder.dateTextView.setText(fertilizerApplication.getDate());
 
-            fertilizerApplicationHolder.operationTextView.setText(fertilizerApplication.getFertilizerName());
+            fertilizerApplicationHolder.operationTextView.setText(fertilizerApplication.getFertilizerId());
             fertilizerApplicationHolder.recurrenceTxt.setText(fertilizerApplication.getRecurrence());
             fertilizerApplicationHolder.rateTextView.setText(fertilizerApplication.getRate()+" kg");
             final ViewTreeObserver observer = fertilizerApplicationHolder.operationTextView.getViewTreeObserver();
@@ -172,7 +172,7 @@ public class CropActivitiesListRecyclerAdapter extends RecyclerView.Adapter< Rec
             final SprayingViewHolder sprayingViewHolder = (SprayingViewHolder)holder;
             CropSpraying field = (CropSpraying)cropsList.get(position);
             sprayingViewHolder.dateTextView.setText(field.getDate());
-            sprayingViewHolder.sprayNameTextView.setText(field.getSprayName());
+            sprayingViewHolder.sprayNameTextView.setText(field.getSprayId());
             sprayingViewHolder.sprayType.setText(field.getSprayType());
             sprayingViewHolder.recurrenceTextView.setText(field.getRecurrence());
             sprayingViewHolder.rateTextView.setText(field.getRate()+" lt");
