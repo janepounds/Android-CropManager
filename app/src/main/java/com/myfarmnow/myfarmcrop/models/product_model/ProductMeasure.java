981e73995fa11e0b85679a32f2b146b3c2192085
DonJovan <mutesasirajovan@gmail.com>
2020-10-16 13:13:36 +0300

    Added product measure model

--- app/src/main/java/com/myfarmnow/myfarmcrop/models/product_model/ProductMeasure.java
+++ app/src/main/java/com/myfarmnow/myfarmcrop/models/product_model/ProductMeasure.java
@@ -7,115 +7,106 @@ import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
 
-public class ProductCategories implements Parcelable {
+public class ProductMeasure implements Parcelable {
 
-    @SerializedName("categories_id")
+    @SerializedName("product_id")
     @Expose
-    private String categoriesId;
-    @SerializedName("categories_name")
+    private String 	product_id;
+
+    @SerializedName("products_weight_unit")
     @Expose
-    private String categoriesName;
-    @SerializedName("categories_image")
+    private String products_weight_unit;
+
+    @SerializedName("products_weight")
     @Expose
-    private String categoriesImage;
-    @SerializedName("categories_icon")
+    private String products_weight;
+
+    @SerializedName("products_price")
     @Expose
-    private String categoriesIcon;
-    @SerializedName("parent_id")
-    @Expose
-    private String parentId;
-    
-    
-    public String getCategoriesId() {
-        return categoriesId;
-    }
-    
-    public void setCategoriesId(String categoriesId) {
-        this.categoriesId = categoriesId;
-    }
-    
-    public String getCategoriesName() {
-        return categoriesName;
-    }
-    
-    public void setCategoriesName(String categoriesName) {
-        this.categoriesName = categoriesName;
-    }
-    
-    public String getCategoriesImage() {
-        return categoriesImage;
-    }
-    
-    public void setCategoriesImage(String categoriesImage) {
-        this.categoriesImage = categoriesImage;
-    }
-    
-    public String getCategoriesIcon() {
-        return categoriesIcon;
-    }
-    
-    public void setCategoriesIcon(String categoriesIcon) {
-        this.categoriesIcon = categoriesIcon;
-    }
-    
-    public String getParentId() {
-        return parentId;
-    }
-    
-    public void setParentId(String parentId) {
-        this.parentId = parentId;
-    }
-    
-    
-    
+    private String products_price;
+
+
     //********** Describes the kinds of Special Objects contained in this Parcelable Instance's marshaled representation *********//
-    
+
     @Override
     public int describeContents() {
         return 0;
     }
-    
-    
-    
+
+
+
     //********** Writes the values to the Parcel *********//
-    
+
     @Override
     public void writeToParcel(Parcel parcel_out, int flags) {
-        parcel_out.writeString(categoriesId);
-        parcel_out.writeString(categoriesName);
-        parcel_out.writeString(categoriesImage);
-        parcel_out.writeString(categoriesIcon);
-        parcel_out.writeString(parentId);
+        parcel_out.writeString(product_id);
+        parcel_out.writeString(products_weight_unit);
+        parcel_out.writeString(products_weight);
+        parcel_out.writeString(products_price);
     }
-    
-    
-    
+
+
+
     //********** Generates Instances of Parcelable class from a Parcel *********//
-    
-    public static final Creator<ProductCategories> CREATOR = new Creator<ProductCategories>() {
+
+    public static final Creator<ProductMeasure> CREATOR = new Creator<ProductMeasure>() {
         // Creates a new Instance of the Parcelable class, Instantiating it from the given Parcel
         @Override
-        public ProductCategories createFromParcel(Parcel parcel_in) {
-            return new ProductCategories(parcel_in);
+        public ProductMeasure createFromParcel(Parcel parcel_in) {
+            return new ProductMeasure(parcel_in);
         }
-        
+
         // Creates a new array of the Parcelable class
         @Override
-        public ProductCategories[] newArray(int size) {
-            return new ProductCategories[size];
+        public ProductMeasure[] newArray(int size) {
+            return new ProductMeasure[size];
         }
     };
-    
-    
-    
+
+
+
     //********** Retrieves the values from the Parcel *********//
-    
-    protected ProductCategories(Parcel parcel_in) {
-        this.categoriesId = parcel_in.readString();
-        this.categoriesName = parcel_in.readString();
-        this.categoriesImage = parcel_in.readString();
-        this.categoriesIcon = parcel_in.readString();
-        this.parentId = parcel_in.readString();
+
+    protected ProductMeasure(Parcel parcel_in) {
+        this.product_id = parcel_in.readString();
+        this.products_weight_unit = parcel_in.readString();
+        this.products_weight = parcel_in.readString();
+        this.products_price = parcel_in.readString();
     }
-    
+
+    public String getProduct_id() {
+        return product_id;
+    }
+
+    public String getProducts_weight_unit() {
+        return products_weight_unit;
+    }
+
+    public String getProducts_weight() {
+        return products_weight;
+    }
+
+    public String getProducts_price() {
+        return products_price;
+    }
+
+    public static Creator<ProductMeasure> getCREATOR() {
+        return CREATOR;
+    }
+
+    public void setProduct_id(String product_id) {
+        this.product_id = product_id;
+    }
+
+    public void setProducts_weight_unit(String products_weight_unit) {
+        this.products_weight_unit = products_weight_unit;
+    }
+
+    public void setProducts_weight(String products_weight) {
+        this.products_weight = products_weight;
+    }
+
+    public void setProducts_price(String products_price) {
+        this.products_price = products_price;
+    }
 }
