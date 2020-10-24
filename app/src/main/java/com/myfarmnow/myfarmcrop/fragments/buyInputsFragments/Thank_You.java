68ec00deb84b1a80062a5a919adf3d8de660b9fc
ONENEMMANUEL <osoema.9@gmail.com>
2020-10-24 12:28:35 +0300

    Designed thank you page

--- app/src/main/java/com/myfarmnow/myfarmcrop/fragments/buyInputsFragments/Thank_You.java
+++ app/src/main/java/com/myfarmnow/myfarmcrop/fragments/buyInputsFragments/Thank_You.java
@@ -1,6 +1,5 @@
 package com.myfarmnow.myfarmcrop.fragments.buyInputsFragments;
 
-
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -10,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,13 +19,14 @@ import androidx.fragment.app.FragmentTransaction;
 
 import com.myfarmnow.myfarmcrop.R;
 
+import org.jetbrains.annotations.NotNull;
+
 import am.appwise.components.ni.NoInternetDialog;
 
-
 public class Thank_You extends Fragment {
-
-    FrameLayout banner_adView;
-    Button order_status_btn, continue_shopping_btn;
+    //    FrameLayout banner_adView;
+    private TextView order_number, estimated_delivery_date;
+    private Button order_status_btn, continue_shopping_btn;
 
     My_Cart my_cart;
 
@@ -40,63 +41,51 @@ public class Thank_You extends Fragment {
 
         // Enable Drawer Indicator with static variable actionBarDrawerToggle of MainActivity
         //MainActivity.actionBarDrawerToggle.setDrawerIndicatorEnabled(false);
-        ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle(getString(R.string.order_confirmed));
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(getString(R.string.order_confirmed));
         NoInternetDialog noInternetDialog = new NoInternetDialog.Builder(getContext()).build();
-       // noInternetDialog.show();
-
+        // noInternetDialog.show();
 
         // Binding Layout Views
-        banner_adView = rootView.findViewById(R.id.banner_adView);
+//        banner_adView = rootView.findViewById(R.id.banner_adView);
+        order_number = rootView.findViewById(R.id.order_number);
+        estimated_delivery_date = rootView.findViewById(R.id.estimated_delivery_date);
         order_status_btn = rootView.findViewById(R.id.order_status_btn);
         continue_shopping_btn = rootView.findViewById(R.id.continue_shopping_btn);
 
-        
-
         // Binding Layout Views
-        order_status_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-    
-                // Navigate to My_Orders Fragment
-                Fragment fragment = new My_Orders();
-                FragmentManager fragmentManager = getFragmentManager();
-                fragmentManager.beginTransaction()
-                        .add(R.id.main_fragment_container, fragment)
-                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
-                        .addToBackStack(null)
-                        .commit();
-                
-            }
+        order_status_btn.setOnClickListener(view -> {
+            // Navigate to My_Orders Fragment
+            Fragment fragment = new My_Orders();
+            FragmentManager fragmentManager = getFragmentManager();
+            fragmentManager.beginTransaction()
+                    .add(R.id.main_fragment_container, fragment)
+                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                    .addToBackStack(null)
+                    .commit();
         });
 
-
         // Binding Layout Views
-        continue_shopping_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Navigate to HomePage Fragment
-                getFragmentManager().popBackStack(getString(R.string.actionHome), FragmentManager.POP_BACK_STACK_INCLUSIVE);
-
-            }
+        continue_shopping_btn.setOnClickListener(view -> {
+            // Navigate to HomePage Fragment
+            getFragmentManager().popBackStack(getString(R.string.actionHome), FragmentManager.POP_BACK_STACK_INCLUSIVE);
         });
-
 
         return rootView;
     }
 
     @Override
-    public void onPrepareOptionsMenu(Menu menu) {
+    public void onPrepareOptionsMenu(@NotNull Menu menu) {
         super.onPrepareOptionsMenu(menu);
     }
 
     @Override
-    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+    public void onCreateOptionsMenu(@NotNull Menu menu, @NotNull MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        
+
         if (item.getItemId() == android.R.id.home) {
             getFragmentManager().popBackStack(getString(R.string.actionHome), FragmentManager.POP_BACK_STACK_INCLUSIVE);
             return true;
@@ -104,8 +93,5 @@ public class Thank_You extends Fragment {
             return super.onOptionsItemSelected(item);
         }
     }
-
-
-
 }
 
